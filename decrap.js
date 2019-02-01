a7ffc0d05ed56669b830f35853f60321fe115302
CK <ckng@ctl.creative.com>
2019-02-01 10:44:10 +0800

    Added Qoo 10

--- decrap.js
+++ decrap.js
@@ -1,12 +1,17 @@
 const amzRegex = /(?:ref|qid|sr|keywords|pd_.*?|pf_.*?|refRID|psc|_encoding).*?(?:\&|$)/gi;
 const googleRegex = /(?:utm_|fbclid)(?:.*)(?:&|\?|\#)/gi;
 const fbRegex = /fbclid\=.*?$/gi;
-const trailingQuestionMark = /\?$/gi;
+const qoo10Regex1 = /(?:__ar|pct|jaehuid).*?(?:\&|$)/gi;
 
-var decrapped = url.replace(amzRegex, '')
+const trailingQuestionMark = /\?$/gi;
+const trailingAmpersand = /\&$/gi;
+
+var decrapped = window.location.toString().replace(amzRegex, '')
   .replace(googleRegex, '')
   .replace(fbRegex, '')
-  .replace(trailingQuestionMark, '');
+  .replace(qoo10Regex1, '')
+  .replace(trailingQuestionMark, '')
+  .replace(trailingAmpersand, '');
 
 console.log('decrapped', decrapped);
 window.history.pushState(null, '', decrapped);
