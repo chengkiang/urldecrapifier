30162caa83a3308f791dfa20ef0d9e92f91eca25
CK <ckng@ctl.creative.com>
2019-02-14 18:35:25 +0800

    Added Medium

--- decrap.js
+++ decrap.js
@@ -7,6 +7,7 @@ function decrap(url) {
     /(?:fbclid|gclid)\=.*?$/gi, // Facebook bbclid / Google gclid
     /(?:__ar|pct|jaehuid).*?(?:\&|$)/gi, // Qoo10
     /(?:version|scm|spm|item_id|abtest|from|acm|pos|abbucket|up_id|wh_weex|mp)\=.*?(?:\&|$)/gi, // Lazada
+    /(?:mkt_tok)\=.*?(?:\&|$)/gi, // Medium
     /(?:\?|\&)$/gi, // Trailing ? or &
   ];
 
