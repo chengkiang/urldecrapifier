8a78ee01fa77ebf56592609e8e6fbf15a739cfb5
CK <ckng@ctl.creative.com>
2019-02-14 16:24:47 +0800

    Fixed overly-greedy Lazada regex'

--- decrap.js
+++ decrap.js
@@ -2,11 +2,11 @@ const url = window.location.toString();
 
 function decrap(url) {
   const regexes = [
-    /(?:ref|qid|sr|keywords|pd_.*?|pf_.*?|refRID|psc|_encoding).*?(?:\&|$)/gi,  // Amazon
+    /(?:ref|qid|sr\=|keywords|pd_.*?|pf_.*?|refRID|psc|_encoding).*?(?:\&|$)/gi,  // Amazon
     /(?:utm_.*?)(\&|$)/gi, // Google utm
     /(?:fbclid|gclid)\=.*?$/gi, // Facebook bbclid / Google gclid
     /(?:__ar|pct|jaehuid).*?(?:\&|$)/gi, // Qoo10
-    /(?:version|scm|spm|item_id|abtest|from|acm|pos|abbucket|up_id|wh_weex|mp\=).*?(?:\&|$)/gi, // Lazada
+    /(?:version|scm|spm|item_id|abtest|from|acm|pos|abbucket|up_id|wh_weex|mp)\=.*?(?:\&|$)/gi, // Lazada
     /(?:\?|\&)$/gi, // Trailing ? or &
   ];
 
