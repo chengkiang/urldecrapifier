f288c7912d96706d24c4892adb71fc4329a91c4c
CK <ckng@ctl.creative.com>
2019-03-18 09:41:09 +0800

    Added a few more Lazada-related querystring to strip off.

--- decrap.js
+++ decrap.js
@@ -6,9 +6,10 @@ function decrap(url) {
     /(?:utm_.*?)(\&|$)/gi, // Google utm
     /(?:fbclid|gclid)\=.*?$/gi, // Facebook bbclid / Google gclid
     /(?:__ar|pct|jaehuid).*?(?:\&|$)/gi, // Qoo10
-    /(?:version|scm|spm|item_id|abtest|from|acm|pos|abbucket|up_id|wh_weex|mp)\=.*?(?:\&|$)/gi, // Lazada
+    /(?:version|scm|spm|item_id|abtest|from|acm|pos|abbucket|up_id|wh_weex|mp|laz_trackid|hybrid)\=.*?(?:\&|$)/gi, // Lazada
     /(?:mkt_tok)\=.*?(?:\&|$)/gi, // Medium,
     /(?:rv|gj|_xtd|_af|_af_eid)\=.*?(?:\&|$)/gi, // Meetup
+    /(?:sc_src|sc_lid|sc_uid|sc_llid|sc_eh)\=.*?(?:\&|$)/gi, // HWZ Mailer
     /(?:\?|\&)$/gi, // Trailing ? or &
   ];
 
