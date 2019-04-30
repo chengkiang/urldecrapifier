586b230707f9cdd9550cc6e44c3859282416e9b6
CK <ckng@ctl.creative.com>
2019-05-01 00:00:57 +0800

    Fixed over-greedy Amazon regex

--- decrap.js
+++ decrap.js
@@ -2,7 +2,7 @@ const url = window.location.toString();
 
 function decrap(url) {
   const regexes = [
-    /(?:ref|qid|sr\=|keywords|pd_.*?|pf_.*?|refRID|psc|_encoding).*?(?:\&|$)/gi,  // Amazon
+    /(?:ref\=|qid|sr\=|keywords|pd_.*?|pf_.*?|refRID|psc|_encoding).*?(?:\&|$)/gi,  // Amazon
     /(?:utm_.*?)(\&|$)/gi, // Google utm
     /(?:fbclid|gclid)\=.*?$/gi, // Facebook bbclid / Google gclid
     /(?:__ar|pct|jaehuid).*?(?:\&|$)/gi, // Qoo10
